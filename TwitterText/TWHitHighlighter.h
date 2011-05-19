66da429e8492689b8f51b7c0e1512dfa7b1bce9f
Sixten Otto <opensource@sfko.com>
2011-05-19 06:14:30 -0700

    Flawed implementation of hit highlighting It works, as far as inserting the new tags at the specified locations, but doesn't implement the "skipping over anchor tags" behavior of the original library

--- TwitterText/TWHitHighlighter.h
+++ TwitterText/TWHitHighlighter.h
@@ -9,8 +9,30 @@
 #import <Foundation/Foundation.h>
 
 
-@interface TWHitHighlighter : NSObject {
+/**
+ * A class for adding HTML highlighting in Tweet text (such as would be returned from a Search)
+ */
+@interface TWHitHighlighter : NSObject
+{
     
 }
+
+/** the current HTML tag used for hit highlighting; defaults to "em" */
+@property (nonatomic, copy) NSString* tag;
+
+/**
+ * Surround the <code>hits</code> in the provided <code>text</code>
+ * with an HTML tag. This is used with ranges from the search API
+ * to support the highlighting of query terms.
+ * 
+ * The text may be auto-linked after the ranges are calculated: this
+ * method will ignore any &lt;a> tags in the text when calculating
+ * the offsets for the highlight tags.
+ *
+ * @param hits An array of ranges within the text to highlight
+ * @param text The text of the Tweet to highlight
+ * @return text The text with highlight HTML added
+ */
+- (NSString *)highlightHits:(NSArray *)hits inText:(NSString *)text;
 
 @end
--- TwitterText/TWHitHighlighter.m
+++ TwitterText/TWHitHighlighter.m
@@ -8,7 +8,75 @@
 
 #import "TWHitHighlighter.h"
 
+NSComparisonResult rangeSort(id r1, id r2, void *context)
+{
+  NSRange v1 = [r1 rangeValue];
+  NSRange v2 = [r2 rangeValue];
+  if (v1.location < v2.location)
+    return NSOrderedAscending;
+  else if (v1.location > v2.location)
+    return NSOrderedDescending;
+  else if (v1.length < v2.length)
+    return NSOrderedAscending;
+  else if (v1.length > v2.length)
+    return NSOrderedDescending;
+  else
+    return NSOrderedSame;
+}
+
+@interface TWHitHighlighter ()
+
+@property (nonatomic, retain) NSString* startTag;
+@property (nonatomic, retain) NSString* endTag;
+
+@end
+
 
 @implementation TWHitHighlighter
+
+@synthesize tag = tw_tag;
+@synthesize startTag = tw_startTag;
+@synthesize endTag = tw_endTag;
+
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.tag = @"em";
+  }
+  return self;
+}
+
+- (void)dealloc {
+  [tw_tag release];
+  [tw_startTag release];
+  [tw_endTag release];
+  [super dealloc];
+}
+
+- (void)setTag:(NSString *)tag {
+  [tw_tag autorelease];
+  tw_tag = [tag copy];
+  self.startTag = [NSString stringWithFormat:@"<%@>", tw_tag];
+  self.endTag   = [NSString stringWithFormat:@"</%@>", tw_tag];
+}
+
+- (NSString *)highlightHits:(NSArray *)hits inText:(NSString *)text {
+  NSArray* sortedRanges = [hits sortedArrayUsingFunction:&rangeSort context:NULL];
+  NSMutableString* highlighted = [text mutableCopy];
+  
+  for( NSValue* val in [sortedRanges reverseObjectEnumerator] ) {
+    NSRange hit = [val rangeValue];
+    NSUInteger end = MIN(NSMaxRange(hit), [highlighted length]);
+    if( hit.location > [highlighted length] ) continue;
+    
+    [highlighted insertString:self.endTag   atIndex:end];
+    [highlighted insertString:self.startTag atIndex:hit.location];
+  }
+  
+  NSString* result = [[highlighted copy] autorelease];
+  [highlighted release];
+  
+  return result;
+}
 
 @end
