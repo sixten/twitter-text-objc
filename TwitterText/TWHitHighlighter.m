d9b3ac01120596fa7fef49f9171e5f0bc7bfd0f6
Sixten Otto <opensource@sfko.com>
2011-05-28 13:14:02 -0700

    Rework highlighter to be aware of embedded anchors

--- TwitterText/TWHitHighlighter.m
+++ TwitterText/TWHitHighlighter.m
@@ -61,20 +61,87 @@ NSComparisonResult rangeSort(id r1, id r2, void *context)
 }
 
 - (NSString *)highlightHits:(NSArray *)hits inText:(NSString *)text {
+  if( [hits count] == 0 ) return [[text copy] autorelease];
+  
   NSArray* sortedRanges = [hits sortedArrayUsingFunction:&rangeSort context:NULL];
   NSMutableString* highlighted = [text mutableCopy];
+  NSMutableArray* anchors = [[NSMutableArray alloc] init];
   
-  for( NSValue* val in [sortedRanges reverseObjectEnumerator] ) {
-    NSRange hit = [val rangeValue];
-    NSUInteger end = MIN(NSMaxRange(hit), [highlighted length]);
-    if( hit.location > [highlighted length] ) continue;
+  NSUInteger index = 0;   // where in the (virtual) text we are
+  NSUInteger aChars = 0;  // how many characters of anchors passed so far
+  NSUInteger hiChars = 0; // how many characters of highlights inserted so far
+  
+  // first, map out all of the anchor tag ranges between the start of the string and the end of the last hit
+  NSRange lastRange = [[sortedRanges lastObject] rangeValue];
+  while( index < NSMaxRange(lastRange) ) {
+    NSRange searchRange = NSMakeRange(index + aChars, [text length] - index - aChars);
+    NSRange anchorRange = [text rangeOfString:@"</?a[^>]*>" options:(NSRegularExpressionSearch|NSCaseInsensitiveSearch) range:searchRange];
+    if( anchorRange.location == NSNotFound ) {
+      break;
+    }
     
-    [highlighted insertString:self.endTag   atIndex:end];
-    [highlighted insertString:self.startTag atIndex:hit.location];
+    [anchors addObject:[NSValue valueWithRange:anchorRange]];
+    index = anchorRange.location - aChars;
+    aChars += anchorRange.length;
+  }
+  //NSLog(@"extracted from »%@« these anchors: %@", text, anchors);
+  
+  // now loop over the hit ranges, using the map to skip over anchor tags in the text
+  NSUInteger currentAnchor = 0;
+  index = 0;
+  aChars = 0;
+  
+  for( NSValue* val in sortedRanges ) {
+    NSRange hit = [val rangeValue];
+    NSUInteger gap = 0;
+    
+    // watch for bad hits
+    if( hit.location + aChars > [text length] )
+      [[NSException exceptionWithName:NSRangeException
+                               reason:@"Specified highlight range outside the bounds of the text"
+                             userInfo:[NSDictionary dictionaryWithObjectsAndKeys:val, @"hit", text, @"text", nil]]
+        raise];
+    
+    // count over any anchors between the end of the last hit, and the start of the current one
+    gap = 0;
+    while( currentAnchor < [anchors count] ) {
+      NSRange anchorRange = [[anchors objectAtIndex:currentAnchor] rangeValue];
+      if( anchorRange.location > hit.location + aChars + gap ) break;
+      gap += anchorRange.length;
+      currentAnchor++;
+    }
+    
+    // add the gap string and the start tag to the new string
+    index = hit.location;
+    aChars += gap;
+    [highlighted insertString:self.startTag atIndex:index+aChars+hiChars];
+    hiChars += [self.startTag length];
+    
+    // watch for over-run
+    if( hit.location + hit.length + aChars + hiChars > [highlighted length] ) {
+      [highlighted appendString:self.endTag];
+      break;
+    }
+    
+    // count over any anchors between the start and end of the hit
+    gap = 0;
+    while( currentAnchor < [anchors count] ) {
+      NSRange anchorRange = [[anchors objectAtIndex:currentAnchor] rangeValue];
+      if( anchorRange.location >= hit.location + hit.length + aChars + gap ) break;
+      gap += anchorRange.length;
+      currentAnchor++;
+    }
+    
+    // add the hit string and the end tag to the new string
+    index = NSMaxRange(hit);
+    aChars += gap;
+    [highlighted insertString:self.endTag atIndex:index+aChars+hiChars];
+    hiChars += [self.endTag length];
   }
   
   NSString* result = [[highlighted copy] autorelease];
   [highlighted release];
+  [anchors release];
   
   return result;
 }
