e4ac3ff58d640e690ae0c510de53bc498e433c28
Sixten Otto <opensource@sfko.com>
2011-05-29 14:20:34 -0700

    Implement basics of TWAutolink, hashtag linking

--- TwitterText/TWAutolink.h
+++ TwitterText/TWAutolink.h
@@ -8,9 +8,113 @@
 
 #import <Foundation/Foundation.h>
 
+/** Default CSS class for auto-linked URLs */
+extern NSString *const TWAutolinkDefaultURLClass;
 
-@interface TWAutolink : NSObject {
+/** Default CSS class for auto-linked list URLs */
+extern NSString *const TWAutolinkDefaultListClass;
+
+/** Default CSS class for auto-linked username URLs */
+extern NSString *const TWAutolinkDefaultUsernameClass;
+
+/** Default CSS class for auto-linked hashtag URLs */
+extern NSString *const TWAutolinkDefaultHashtagClass;
+
+/** Default href for username links (the username without the @ will be appended) */
+extern NSString *const TWAutolinkDefaultUsernameURLBase;
+
+/** Default href for list links (the username/list without the @ will be appended) */
+extern NSString *const TWAutolinkDefaultListURLBase;
+
+/** Default href for hashtag links (the hashtag without the # will be appended) */
+extern NSString *const TWAutolinkDefaultHashtagURLBase;
+
+/** HTML attribute to add when noFollow is true (default) */
+extern NSString *const TWAutolinkNoFollowAttribute;
+
+
+/**
+ * A class for adding HTML links to hashtag, username and list references in Tweet text.
+ */
+@interface TWAutolink : NSObject
+{
     
 }
+
+/**
+ * The CSS class for auto-linked URLs
+ */
+@property (nonatomic, copy) NSString* urlClass;
+
+/**
+ * The CSS class for auto-linked list URLs
+ */
+@property (nonatomic, copy) NSString* listClass;
+
+/**
+ * The CSS class for auto-linked username URLs
+ */
+@property (nonatomic, copy) NSString* usernameClass;
+
+/**
+ * The CSS class for auto-linked hashtag URLs
+ */
+@property (nonatomic, copy) NSString* hashtagClass;
+
+/**
+ * The href value for username links (to which the username will be appended)
+ */
+@property (nonatomic, copy) NSString* usernameUrlBase;
+
+/**
+ * The href value for list links (to which the username/list will be appended)
+ */
+@property (nonatomic, copy) NSString* listUrlBase;
+
+/**
+ * The href value for hashtag links (to which the hashtag will be appended)
+ */
+@property (nonatomic, copy) NSString* hashtagUrlBase;
+
+/**
+ * Whether the current URL links will include rel="nofollow" (YES by default)
+ */
+@property (nonatomic, assign) BOOL noFollow;
+
+
+/**
+ * Auto-link hashtags, URLs, usernames and lists.
+ *
+ * @param text The text of the Tweet to auto-link
+ * @return The text with auto-link HTML added
+ */
+- (NSString *)autoLink:(NSString *)text;
+
+/**
+ * Auto-link the @username and @username/list references in the provided text. Links to @username references will
+ * have the usernameClass CSS classes added. Links to @username/list references will have the listClass CSS class
+ * added.
+ *
+ * @param text The text of the Tweet to auto-link
+ * @return The text with auto-link HTML added
+ */
+- (NSString *)autoLinkUsernamesAndLists:(NSString *)text;
+
+/**
+ * Auto-link #hashtag references in the provided Tweet text. The #hashtag links will have the hashtagClass CSS class
+ * added.
+ *
+ * @param text The text of the Tweet to auto-link
+ * @return The text with auto-link HTML added
+ */
+- (NSString *)autoLinkHashtags:(NSString *)text;
+
+/**
+ * Auto-link URLs in the Tweet text provided.
+ *
+ * @param text The text of the Tweet to auto-link
+ * @return The text with auto-link HTML added
+ */
+- (NSString *)autoLinkURLs:(NSString *)text;
 
 @end
--- TwitterText/TWAutolink.m
+++ TwitterText/TWAutolink.m
@@ -8,7 +8,76 @@
 
 #import "TWAutolink.h"
 
+#import "TWRegex.h"
+
+#import "RegexKitLite.h"
+
+
+NSString *const TWAutolinkDefaultURLClass = @"tweet-url";
+NSString *const TWAutolinkDefaultListClass = @"list-slug";
+NSString *const TWAutolinkDefaultUsernameClass = @"username";
+NSString *const TWAutolinkDefaultHashtagClass = @"hashtag";
+NSString *const TWAutolinkDefaultUsernameURLBase = @"http://twitter.com/";
+NSString *const TWAutolinkDefaultListURLBase = @"http://twitter.com/";
+NSString *const TWAutolinkDefaultHashtagURLBase = @"http://twitter.com/search?q=%23";
+NSString *const TWAutolinkNoFollowAttribute = @" rel=\"nofollow\"";
+
+
+
 
 @implementation TWAutolink
+
+@synthesize urlClass = tw_urlClass;
+@synthesize listClass = tw_listClass;
+@synthesize usernameClass = tw_usernameClass;
+@synthesize hashtagClass = tw_hashtagClass;
+@synthesize usernameUrlBase = tw_usernameUrlBase;
+@synthesize listUrlBase = tw_listUrlBase;
+@synthesize hashtagUrlBase = tw_hashtagUrlBase;
+@synthesize noFollow = tw_noFollow;
+
+- (id)init {
+  self = [super init];
+  if (self) {
+    self.urlClass = TWAutolinkDefaultURLClass;
+    self.listClass = TWAutolinkDefaultListClass;
+    self.usernameClass = TWAutolinkDefaultUsernameClass;
+    self.hashtagClass = TWAutolinkDefaultHashtagClass;
+    self.usernameUrlBase = TWAutolinkDefaultUsernameURLBase;
+    self.listUrlBase = TWAutolinkDefaultListURLBase;
+    self.hashtagUrlBase = TWAutolinkDefaultHashtagURLBase;
+    self.noFollow = YES;
+  }
+  return self;
+}
+
+- (void)dealloc {
+  [tw_urlClass release];
+  [tw_listClass release];
+  [tw_usernameClass release];
+  [tw_hashtagClass release];
+  [tw_usernameUrlBase release];
+  [tw_listUrlBase release];
+  [tw_hashtagUrlBase release];
+  [super dealloc];
+}
+
+- (NSString *)autoLink:(NSString *)text {
+  return [self autoLinkUsernamesAndLists:[self autoLinkURLs:[self autoLinkHashtags:text]]];
+}
+
+- (NSString *)autoLinkUsernamesAndLists:(NSString *)text {
+  return nil;
+}
+
+- (NSString *)autoLinkHashtags:(NSString *)text {
+  NSString* replacement = [NSString stringWithFormat:@"$%i<a href=\"%@$%i\" title=\"#$%i\" class=\"%@ %@\"%@>$%i$%i</a>", TWRegexGroupsAutoLinkHashtagBefore, self.hashtagUrlBase, TWRegexGroupsAutoLinkHashtagTag, TWRegexGroupsAutoLinkHashtagTag, self.urlClass, self.hashtagClass, (self.noFollow ? TWAutolinkNoFollowAttribute : @""), TWRegexGroupsAutoLinkHashtagHash, TWRegexGroupsAutoLinkHashtagTag];
+  
+  return [text RKL_METHOD_PREPEND(stringByReplacingOccurrencesOfRegex):[TWRegex autoLinkHashtags] withString:replacement];
+}
+
+- (NSString *)autoLinkURLs:(NSString *)text {
+  return nil;
+}
 
 @end
