e87e014246dd8bba3907a9ec1517d41dd4e8a751
Sixten Otto <opensource@sfko.com>
2011-10-08 17:08:37 -0700

    Fix TLD regexs to work with ICU Some character properties (like {Alpha}) in Java are ASCII-only, while in ICU they are multicultural, and include things like CJK ideographs. So, replace them with old-school character classes.

--- TwitterText/TWRegex.m
+++ TwitterText/TWRegex.m
@@ -19,7 +19,7 @@ static NSString *const TWHashtagNumericChars = @"0-9\uff10-\uff19_";
 static NSString *const URL_VALID_PRECEEDING_CHARS = @"(?:[^\\-/\"'!=A-Z0-9_@＠.\u202A-\u202E]|^)";
 static NSString *const URL_VALID_UNICODE_CHARS = @"[.[^\\p{Punct}\\s\\p{Z}\\p{InGeneralPunctuation}]]";
 static NSString *const URL_VALID_GTLD =
-  @"(?:(?:aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel)(?=\\P{Alpha}|$))";
+  @"(?:(?:aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel)(?=[^a-zA-Z]|$))";
 static NSString *const URL_VALID_CCTLD =
   @"(?:(?:ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|"
   @"bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|"
@@ -28,7 +28,7 @@ static NSString *const URL_VALID_CCTLD =
   @"lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|"
   @"nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|"
   @"sl|sm|sn|so|sr|ss|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|"
-  @"va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)(?=\\P{Alpha}|$))";
+  @"va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)(?=[^a-zA-Z]|$))";
 static NSString *const URL_PUNYCODE = @"(?:xn--[0-9a-z]+)";
 static NSString *const URL_VALID_PORT_NUMBER = @"[0-9]++";
 static NSString *const URL_VALID_URL_QUERY_CHARS = @"[a-z0-9!\\*'\\(\\);:&=\\+\\$/%#\\[\\]\\-_\\.,~\\|]";
